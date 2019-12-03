35464ec7816923f622074ec5d94b9fd178243304
Alex <alex.foster@moo.com>
2019-12-03 08:26:37 +0000

    Nice log function

---
+++ log.js
@@ -0,0 +1,9 @@
+module.exports = (result) => {
+    console.log('🎁 🎄 🎁 🎄 🎁 🎄 🎁 🎄 🎁 🎄 🎁 🎄 🎁 🎄 🎁');
+    console.log('');
+    console.log('🎄', '\t\t\t\t\t', ' 🎄');
+    console.log('\t', 'Result:', result);
+    console.log('🎁', '\t\t\t\t\t', ' 🎁');
+    console.log('');
+    console.log('🎄 🎁 🎄 🎁 🎄 🎁 🎄 🎁 🎄 🎁 🎄 🎁 🎄 🎁 🎄');
+};
